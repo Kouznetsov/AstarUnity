e72c7418d54a016e86a66a712e8e91041c5619d2
Karim <karim@chouic.com>
2020-10-29 10:31:50 +0100

    ok

--- Assets/Astar.cs
+++ Assets/Astar.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 public class Astar : MonoBehaviour
 {
     private List<Node> _grid;
-
+    
     private void Start()
     {
         _grid = GetComponent<GridGenerator>().grid;
+        _grid.Find(n => n.status == NodeStatus.START).status = NodeStatus.OPEN;
     }
 
     void NextStep()
     {
-        
+        //var lowestFcost = _grid.Count;
+        // TODO
     }
 
     private void Update()
